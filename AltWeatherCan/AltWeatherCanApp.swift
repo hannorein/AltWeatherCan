2b09f558af7a7fc9708cbf1050eab1d7001437b7
Hanno Rein <hanno@hanno-rein.de>
2024-10-26 14:57:16 -0400

    Initial Commit

---
+++ AltWeatherCan/AltWeatherCanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AltWeatherCanApp.swift
+//  AltWeatherCan
+//
+//  Created by Hanno Rein on 2024-10-26.
+//
+
+import SwiftUI
+
+@main
+struct AltWeatherCanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
