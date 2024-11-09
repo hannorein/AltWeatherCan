7b46c3909f16b88a85c5d4eab18673b45dec8c82
Hanno Rein <hanno@hanno-rein.de>
2024-11-08 21:14:36 -0500

    Remembers previous 10 selected stations

--- AltWeatherCan/AppManager.swift
+++ AltWeatherCan/AppManager.swift
@@ -24,12 +24,17 @@ class AppManager : ObservableObject {
 //    @Published var selectedSite = Site(code: "s0000458", name: "Toronto", province: "ON", latitude: 43.74, longitude: 79.37)
 //    @Published var selectedSite = Site(code: "s0000630", name: "Port Perry", province: "ON", latitude: 43.74, longitude: 79.37)
     @Published var selectedSite = Site(code: "s0000627", name: "Inukjuak", province: "QC", latitude: 43.74, longitude: 79.37)
+    @Published var previousSites : [Site] = []
     
     init() {
         let defaults = UserDefaults.standard
         if let contentData = defaults.object(forKey: "defaultSite") as? Data,
             let defaultSite = try? JSONDecoder().decode(Site.self, from: contentData) {
             selectedSite = defaultSite
+        }
+        if let contentData = defaults.object(forKey: "previousSites") as? Data,
+            let _previousSites = try? JSONDecoder().decode([Site].self, from: contentData) {
+            previousSites = _previousSites
         }
        
         DispatchQueue.global().async {
@@ -55,12 +60,47 @@ class AppManager : ObservableObject {
                     if let contentData = try? JSONEncoder().encode(self.selectedSite) {
                         defaults.set(contentData, forKey: "defaultSite")
                     }
+                    // Store list of previous sites
+                    self.previousSites.removeAll { s in
+                        s.code == self.selectedSite.code
+                    }
+                    
+                    self.previousSites.insert(self.selectedSite, at: 0)
+                    if self.previousSites.count > 10 {
+                        self.previousSites.removeLast()
+                    }
+                    // Re-sort (puts recent ones at top)
+                    self.sortSiteList()
+                    
+                    if let contentData = try? JSONEncoder().encode(self.previousSites) {
+                        defaults.set(contentData, forKey: "previousSites")
+                    }else{
+                        print("encoding error previousSites")
+                    }
                 } catch {
                     print("decoding error: \(error)")
                 }
             }
         }catch {
             print("download error: \(error)")
+        }
+    }
+    
+    func sortSiteList() {
+        if let sites = self.sites {
+            self.sites = sites.sorted(by: { a, b in
+                return a.name < b.name
+            })
+            // Move previous selection to top of list.
+            for site in previousSites.reversed() {
+                if let index = self.sites?.firstIndex(where: { s in
+                    s.code == site.code
+                }){
+                    if let s = self.sites?.remove(at: index){
+                        self.sites?.insert(s, at: 0)
+                    }
+                }
+            }
         }
     }
     
@@ -84,9 +124,8 @@ class AppManager : ObservableObject {
                         newSites.append(site)
                     }
                 }
-                self.sites = newSites.sorted(by: { a, b in
-                    a.name < b.name
-                })
+                self.sites = newSites
+                self.sortSiteList()
             }
         }catch {
             print("download error: \(error)")
