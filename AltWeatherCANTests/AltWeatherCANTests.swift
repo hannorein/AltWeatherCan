1a34f2371b4569b43aaa4e510a30eb368975ad38
Hanno Rein <hanno@hanno-rein.de>
2024-11-09 15:55:14 -0500

    Added unit tests. Refactored Site to Site.swift.

--- AltWeatherCANTests/AltWeatherCANTests.swift
+++ AltWeatherCANTests/AltWeatherCANTests.swift
@@ -6,11 +6,26 @@
 //
 
 import Testing
+import Foundation
+import XMLCoder
 
 struct AltWeatherCANTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func getAllSites() async throws {
+        let sites = Site.getAvailableSites()
+        try #require(sites.count > 0, "No sites found.")
+        #expect(sites.count(where: { s in
+            s.name == "Toronto"
+        }) == 1, "Toronto not found.")
+        
+        // Getting and parsing all station data. This may take a while.
+        for (index, site) in sites.enumerated() {
+            let stationUrl = "https://dd.weather.gc.ca/citypage_weather/xml/"+site.province+"/"+site.code+"_e.xml"
+            print("Getting \(index+1)/\(sites.count): \(stationUrl)")
+            let sourceXML = try String(contentsOf: URL(string: stationUrl)!, encoding: .ascii)
+            
+            let citypage = try XMLDecoder().decode(Citypage.self, from: Data(sourceXML.utf8))
+        }
     }
 
 }
--- AltWeatherCan/AppManager.swift
+++ AltWeatherCan/AppManager.swift
@@ -8,38 +8,6 @@
 import Foundation
 import XMLCoder
 
-struct Site : Identifiable, Hashable, Codable {
-    var id = UUID()
-    let code : String
-    let name : String
-    let province : String
-    let latitude : Double?
-    let longitude : Double?
-    
-    static func getAvailableSites() -> [Site] {
-        var newSites: [Site] = []
-        do {
-            let sourceCSV = try String(contentsOf: URL(string: "https://dd.weather.gc.ca/citypage_weather/docs/site_list_en.csv")!)
-                var rows = sourceCSV.components(separatedBy: "\n")
-
-                rows.removeFirst()
-                rows.removeFirst()
-
-                for row in rows {
-                    let columns = row.components(separatedBy: ",")
-                    if columns.count >= 5 {
-                        let latitude = Double(columns[3].replacingOccurrences(of: "N", with: ""))
-                        let longitude = Double("-"+columns[4].replacingOccurrences(of: "W", with: "")) // Hard coded for Canada
-                        let site = Site(code: columns[0], name: columns[1], province: columns[2], latitude: latitude, longitude: longitude)
-                        newSites.append(site)
-                    }
-            }
-        }catch {
-            print("download error: \(error)")
-        }
-        return newSites
-    }
-}
 
 class AppManager : ObservableObject {
     
